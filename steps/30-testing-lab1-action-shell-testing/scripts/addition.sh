b281b90b2052849e700ae19d347693efc3978283
Romain Lespinasse <romain.lespinasse1.partner@decathlon.com>
2022-09-21 01:55:09 +0200

    testing lab1 cleanup

--- steps/30-testing-lab1-action-shell-testing/scripts/addition.sh
+++ steps/30-testing-lab1-action-shell-testing/scripts/addition.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 2 ]
-then
-    echo "command line arguments are missing"
-    exit 3
+if [ $# -lt 2 ]; then
+  echo "command line arguments are missing"
+  exit 3
 else
-    echo $(($1+$2))
+  echo $(($1 + $2))
 fi
